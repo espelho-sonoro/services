e69a9491cae1886d3ec0d4d57a1c33bea18a86ad
Luppi <luppijr@gmail.com>
2018-01-29 21:49:55 -0200

    captura de imagens a cada 1min

--- tweet.py
+++ tweet.py
@@ -1,5 +1,7 @@
 from twython import Twython, TwythonError
-
+from time import sleep
+from picamera import PiCamera
+from datetime import datetime, timedelta
 
 
 d = {}
@@ -9,14 +11,24 @@ with open("twitter_keys") as f:
        d[key] = val
 print d
 
-
-
-# Requires Authentication as of Twitter API v1.1
 twitter = Twython(d['APP_KEY'], d['APP_SECRET'], d['OAUTH_TOKEN'], d['OAUTH_TOKEN_SECRET'])
 
-try:
-    twitter.update_status(status='Ola pessoal')
-except TwythonError as e:
-    print e
+def wait():
+    # Calculate the delay to the start of the next hour
+    next_hour = (datetime.now() + timedelta(minutes=1)).replace(
+       second=0, microsecond=0)
+    delay = (next_hour - datetime.now()).seconds
+    sleep(delay)
 
+camera = PiCamera()
+camera.start_preview()
+wait()
+for filename in camera.capture_continuous('img/img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
+    print('Captured %s' % filename)
+    try:
+        twitter.update_status_with_media(status=message, media=photo)
+    except TwythonError as e:
+        print e
+    with open('/home/pi/Downloads/image.jpg', 'rb') as photo:
+    wait()
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
