f2b700e4ffa8c8ac9bf2f8de46289e55ec51190e
Luppi <luppijr@gmail.com>
2018-01-29 23:22:48 -0200

    concluido o script de captura de imagem e publicacao no twitter

--- tweet.py
+++ tweet.py
@@ -1,34 +1,32 @@
 from twython import Twython, TwythonError
 from time import sleep
 from picamera import PiCamera
-from datetime import datetime, timedelta
+from datetime import datetime, date, time, timedelta
 
+delay = 1800  #delay in seconds
 
 d = {}
 with open("twitter_keys") as f:
     for line in f:
        (key, val) = line.split()
        d[key] = val
-print d
 
 twitter = Twython(d['APP_KEY'], d['APP_SECRET'], d['OAUTH_TOKEN'], d['OAUTH_TOKEN_SECRET'])
 
-def wait():
-    # Calculate the delay to the start of the next hour
-    next_hour = (datetime.now() + timedelta(minutes=1)).replace(
-       second=0, microsecond=0)
-    delay = (next_hour - datetime.now()).seconds
-    sleep(delay)
 
 camera = PiCamera()
 camera.start_preview()
-wait()
+sleep(delay)
 for filename in camera.capture_continuous('img/img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
     print('Captured %s' % filename)
-    try:
-        twitter.update_status_with_media(status=message, media=photo)
-    except TwythonError as e:
-        print e
-    with open('/home/pi/Downloads/image.jpg', 'rb') as photo:
-    wait()
+    ts = datetime.now()
+    message = '[' + str(ts.strftime("%Y-%m-%d %H:%M"))  + '] Espelho Sonoro transmitindo ao vivo! Confira em http://www.espelhosonoro.com/'
+    with open(filename, 'rb') as photo:
+        try:
+#            twitter.update_status_with_media(status=message, media=photo)
+            response = twitter.upload_media(media=photo)
+            twitter.update_status(status=message, media_ids=[response['media_id']])
+        except TwythonError as e:
+            print e
+    sleep(delay)
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
