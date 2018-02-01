444a4ac6e72e1cd0319c0b4d5c62e808698ae015
Luppi <luppijr@gmail.com>
2018-02-01 03:28:52 -0200

    gerador de arquivos

--- autostream.sh
+++ autostream.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-sudo usb_modeswitch -c /etc/usb_modeswitch.conf
-sleep 1s
-sudo wvdial &
+#sudo usb_modeswitch -c /etc/usb_modeswitch.conf
+#sleep 1s
+#sudo wvdial &
+#sed '/localDumpFile/ s/$/ $filename/' /home/pi/services/darkice.cfg.bak
+sed "/localDumpFile/ s/$/$(date '+%Y_%m_%d__%H_%M_%S').ogg/" /home/pi/services/template.cfg | tee /home/pi/services/darkice.cfg
+
+#sed -i -e '/localDumpFile/ =/ s/= .*/= \1$filename/' /home/pi/services/darkice.cfg.bak
 darkice -c /home/pi/darkice.cfg
